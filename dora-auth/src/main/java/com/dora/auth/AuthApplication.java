6ba8135787fae581e3736a0dae74855cc5ba97a9
zhou <zhou>
2021-09-28 17:47:57 +0800

    security

---
+++ dora-auth/src/main/java/com/dora/auth/AuthApplication.java
@@ -0,0 +1,15 @@
+package com.dora.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhou
+ * @date 2021/9/28
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ dora-auth/src/main/java/com/dora/auth/config/AuthorizationServerConfig.java
@@ -0,0 +1,24 @@
+package com.dora.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhou
+ * @date 2021/9/28
+ */
+@Configuration
+public class AuthorizationServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .mvcMatcher("/messages/**")
+                .authorizeRequests()
+                .mvcMatchers("/messages/**").access("hasAuthority('SCOPE_message.read')")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+}
